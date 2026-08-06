22f35afcb4d7f375735c64054f9e7afb5144e9f0
mahmudulhasanzb <mahmudulhasankk9@gmail.com>
2026-08-06 16:35:19 +0600

    updated: updated navbar added dashboard route if use avalaible

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { Menu, X } from 'lucide-react'
+import { authClient } from '@/src/lib/auth-client'
 
 const navLinks = [
   { label: 'Home', path: '/' },
@@ -14,6 +15,8 @@ const navLinks = [
 ]
 
 const Navbar = () => {
+  const { data: session } = authClient.useSession()
+  const user = session?.user
   const pathname = usePathname()
   const [isOpen, setIsOpen] = useState(false)
 
@@ -22,8 +25,10 @@ const Navbar = () => {
   return (
     <nav className="absolute top-0 left-0 w-full z-50 bg-transparent py-5 px-6 md:px-12 flex items-center justify-between">
       {/* Brand Logo - Left Side */}
-      <Link href="/" className="flex items-center gap-2 group cursor-pointer select-none">
-        
+      <Link
+        href="/"
+        className="flex items-center gap-2 group cursor-pointer select-none"
+      >
         <span className="font-extrabold text-2xl tracking-tight text-white">
           Food<span className="text-yellow-400">Zen</span>
         </span>
@@ -31,8 +36,8 @@ const Navbar = () => {
 
       {/* Middle Navigation Routes - Desktop */}
       <div className="hidden md:flex items-center gap-8">
-        {navLinks.map((item) => {
-          const isActive = pathname === item.path
+        {navLinks.map(item => {
+          const isActive = pathname === item.path;
           return (
             <Link
               key={item.path}
@@ -50,18 +55,27 @@ const Navbar = () => {
                 }`}
               />
             </Link>
-          )
+          );
         })}
       </div>
 
       {/* Sign Up Button - Right Side (Desktop) */}
       <div className="hidden md:block">
+       {user ? (
+         <Link
+          href="/dashboard"
+          className="bg-yellow-400 text-red-950 font-bold px-6 py-2.5 rounded-full hover:bg-yellow-300 hover:shadow-lg hover:shadow-yellow-400/25 active:scale-95 transition-all duration-200 cursor-pointer inline-block"
+        >
+          Dashboard
+        </Link>
+       ) : (
         <Link
           href="/signup"
           className="bg-yellow-400 text-red-950 font-bold px-6 py-2.5 rounded-full hover:bg-yellow-300 hover:shadow-lg hover:shadow-yellow-400/25 active:scale-95 transition-all duration-200 cursor-pointer inline-block"
         >
           Sign Up
         </Link>
+       )}
       </div>
 
       {/* Hamburger Menu - Mobile */}
@@ -79,8 +93,8 @@ const Navbar = () => {
       {isOpen && (
         <div className="absolute top-full left-0 right-0 mt-2 mx-4 bg-red-950/95 backdrop-blur-md border border-red-800/40 rounded-2xl p-6 shadow-2xl flex flex-col gap-5 md:hidden animate-in fade-in slide-in-from-top-5 duration-200">
           <div className="flex flex-col gap-4">
-            {navLinks.map((item) => {
-              const isActive = pathname === item.path
+            {navLinks.map(item => {
+              const isActive = pathname === item.path;
               return (
                 <Link
                   key={item.path}
@@ -94,20 +108,30 @@ const Navbar = () => {
                 >
                   {item.label}
                 </Link>
-              )
+              );
             })}
           </div>
-          <Link
-            href="/signup"
-            onClick={() => setIsOpen(false)}
-            className="bg-yellow-400 text-red-950 text-center font-bold py-3 rounded-full hover:bg-yellow-300 active:scale-98 transition-all duration-200 cursor-pointer"
-          >
-            Sign Up
-          </Link>
+          {user ? (
+            <Link
+                href={'/dashboard'}
+                onClick={() => setIsOpen(false)}
+                className="font-semibold text-lg transition-colors duration-200 py-1.5 border-b border-red-900/30 cursor-pointer text-white/80 hover:text-yellow-400"
+              >
+                Dashboard
+              </Link>
+          ) : (
+            <Link
+              onClick={() => setIsOpen(false)}
+              href="/signup"
+              className="font-semibold text-lg transition-colors duration-200 py-1.5 border-b border-red-900/30 cursor-pointer text-white/80 hover:text-yellow-400"
+            >
+              Sign Up
+            </Link>
+          )}
         </div>
       )}
     </nav>
-  )
+  );
 }
 
 export default Navbar
